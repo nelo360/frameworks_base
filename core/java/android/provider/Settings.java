1419a5db14cc5c840f95e51614deffe228249180
Der-Schubi <schubi@erlangen.cc.de>
2014-01-11 18:41:17 +0100

    [1/2] LS Notification dinamic width & blacklist options
    - Lockscreen Notifications: disable dynamic width for devices <= xhdpi
    
    - [1/2] Base: Lockscreen notifications: Option to disable dynamic width
    from: https://github.com/OmniKang/android_frameworks_base/commit/87d6598b63a1b83e93631b0fcb0822365cf7563b
    
    -  [1/2] Base: Lockscreen notifications: Allow apps to be excluded from being displayed
    from: https://github.com/OmniKang/android_frameworks_base/commit/8631349b965f507eeff4b5616c1f581bc4e6e615
    
     Credits to 0xD34D / ChameleonOS for the original implementation for Active Display
    
    PS3/4: fixes
    
    Change-Id: I796b08cb94185ea17139bd74f1b41d287ba65155
    
    Conflicts:
    
    	packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3391,6 +3391,19 @@ public final class Settings {
         */
         public static final String NOTIFICATION_ALPHA = "notification_alpha";
 
+       /**
+        * Enables dynamic with for shown notifications on devices >= xxhdpi
+        * @hide
+        */
+        public static final String LOCKSCREEN_NOTIFICATIONS_DYNAMIC_WIDTH = "lockscreen_notifications_dynamic_width";
+
+       /**
+        * A list of packages to exclude from being displayed as lockscreen notifications.
+        * This should be a string of packages separated by |
+        * @hide
+        */
+        public static final String LOCKSCREEN_NOTIFICATIONS_EXCLUDED_APPS = "lockscreen_notifications_excluded_apps";
+
         /**
          * TeloRadio enable
          * @hide
