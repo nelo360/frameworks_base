c12d129b4e6b1a2c577f8f1c4fa28f91270c26a7
maxwen <max.weninger@gmail.com>
2014-01-19 23:06:31 +0100

    [1/2] Frameworks: Wakelock Blocker
    Conflicts:
    
    	core/java/android/os/IPowerManager.aidl
    	core/java/android/provider/Settings.java
    	services/java/com/android/server/power/PowerManagerService.java
    
    Change-Id: Id4fd1d8da6ed40517094a5a156f7c6fea622e985
    
    Conflicts:
    	core/java/android/os/IPowerManager.aidl
    	core/java/android/provider/Settings.java
    
    Conflicts:
    
    	core/java/android/os/IPowerManager.aidl

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4262,6 +4262,18 @@ public final class Settings {
         public static final String CUSTOM_RECENT = "custom_recent";
 
         /**
+         * Enable blocking wakelock
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+        /**
+         * List of wakelock blocks selected
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
