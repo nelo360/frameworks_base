59c4bbf449b41ffe6a6717e245a17835849a9b47
D4rKn3sSyS <jesdga95@gmail.com>
2014-03-09 00:23:17 -0500

    PEEK [1/2]
    Sensors are fully disabled when screen is on. When you turn the screen off,
    it will start polling both gyroscope and proximity sensor values. If it detects
    using the gyroscope sensor that device is lying on a table, it disables proximity
    polling. If it detects with proximity sensor that it is in your pocket, it disables
    gyroscope polling. This way, we save some battery from the beginning. Later, when you
    receive a notification, it enables a partial wakelock (CPU on but at low frequency),
    and starts listening for movement, either from getting it out your pocket or lifting
    off from the table. If it detects those events, it will turn on screen and show the notification.
    Wakelock timeouts on 10 secs, so if you receive a notification and there is no movement in that time,
    sensors will stop polling after that time, disabling peek interaction until a new notification comes.
    Peek doesn't process low and min priority notifications.
    
    https://plus.google.com/+ParanoidAndroidCorner/posts/3wppxakBSVR
    
    Contributors:
    @D4rKn3sSyS
    @Anu6is
    @BigBrother1984
    
    Signed-off-by: Jesus David <jesdga95@gmail.com>
    Signed-off-by: Curtis Mayers <curtis.mayers@gmail.com>
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    Conflicts:
    	packages/SystemUI/res/values/pa_dimens.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    
    Change-Id: Ied2b76ffc236d46e20ca76082984843f03229045
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4377,6 +4377,13 @@ public final class Settings {
          */
         public static final String NOTIFICATION_BRIGHTNESS_SLIDER = "notification_brightness_slider";
 
+        /**
+         * Either if notification peek is enabled of disabled
+         * 0 = disabled (default)
+         * 1 = enabled
+         * @hide
+         */
+        public static final String PEEK_STATE = "peek_state";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
