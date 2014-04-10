f2082c96a12ccd8c17306375296678f2b159aa5d
Roman Birg <roman@cyngn.com>
2014-04-10 16:03:30 -0700

    Keyguard: more consistency with the new lockscreen wallpaper
    * In the case when the screen was off and setCustomDrawable()
    was being called, and the use wallpaper is null, then we could be
    setting the color filter on a null drawable.
    
    * Fix logic when old custom background was null and the new one is also
    null (but could be the user one) and the func was being returned, thus not
    clearing out the current media album art.
    
    * Move resetting the shouldShowWallpaper() flag to the show() method
    so it is not being called every time the screen is turned on.
    
    * Fix user image not showing up on first boot if it exists
    
    Change-Id: I567c2ec8920df627835defb822c2ea25be7b3cab
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -195,6 +195,8 @@ public class KeyguardViewManager {
 
         maybeCreateKeyguardLocked(rotationAngles, false, options);
         maybeEnableScreenRotation(rotationAngles);
+        updateShowWallpaper(mKeyguardHost.shouldShowWallpaper());
+
 
         // Disable common aspects of the system/status/navigation bars that are not appropriate or
         // useful on any keyguard screen but can be re-shown by dialogs or SHOW_WHEN_LOCKED
@@ -263,7 +265,6 @@ public class KeyguardViewManager {
         public ViewManagerHost(Context context) {
             super(context);
             setBackground(mBackgroundDrawable);
-            cacheUserImage();
         }
 
         public void drawToCanvas(Canvas canvas, Drawable drawable) {
@@ -286,20 +287,24 @@ public class KeyguardViewManager {
             if (!ActivityManager.isHighEndGfx() || !mScreenOn) {
                 if (d == null) {
                     d = mUserBackground;
-                } else {
-                    d.setColorFilter(BACKGROUND_COLOR, PorterDuff.Mode.SRC_OVER);
                 }
+                // no user wallpaper set
+                if (d == null) {
+                    d = new ColorDrawable(BACKGROUND_COLOR);
+                }
+                d.setColorFilter(BACKGROUND_COLOR, PorterDuff.Mode.SRC_OVER);
                 mCustomBackground = d;
                 computeCustomBackgroundBounds(mCustomBackground);
                 setBackground(mBackgroundDrawable);
             } else {
                 Drawable old = mCustomBackground;
-                if (old == null && d == null) {
+                if (old == null && d == null && mUserBackground == null) {
                     return;
                 }
                 boolean newIsNull = mUserBackground == null;
                 if (old == null) {
                     old = new ColorDrawable(BACKGROUND_COLOR);
+                    old.setColorFilter(BACKGROUND_COLOR, PorterDuff.Mode.SRC_OVER);
                 }
                 if (d == null) {
                     d = mUserBackground;
@@ -471,6 +476,12 @@ public class KeyguardViewManager {
             mViewManager.addView(mKeyguardHost, lp);
 
             KeyguardUpdateMonitor.getInstance(mContext).registerCallback(mBackgroundChanger);
+            mKeyguardHost.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mKeyguardHost.cacheUserImage();
+                }
+            }, 100);
         }
 
         if (force || mKeyguardView == null) {
@@ -647,7 +658,6 @@ public class KeyguardViewManager {
 
         if (mKeyguardView != null) {
             mKeyguardView.onScreenTurnedOn();
-            updateShowWallpaper(mKeyguardHost.shouldShowWallpaper());
 
             // Caller should wait for this window to be shown before turning
             // on the screen.
