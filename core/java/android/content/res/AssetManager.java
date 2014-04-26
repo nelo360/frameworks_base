ef93a943bb90c3d0bb0c62e93b9a3b0b98aea121
Andy Mast <andy@cyngn.com>
2014-04-25 17:28:41 -0700

    hide new AssetManager methods for themes
    Change-Id: I9f5c6a432f9c2f3c1dd5bd9927c5e3fe3ddd1f93

--- core/java/android/content/res/AssetManager.java
+++ core/java/android/content/res/AssetManager.java
@@ -821,10 +821,12 @@ public final class AssetManager {
         mThemeCookies.add(cookie);
     }
 
+    /** {@hide} */
     public String getAppName() {
         return mAppName;
     }
 
+    /** {@hide} */
     public void setAppName(String pkgName) {
         mAppName = pkgName;
     }
