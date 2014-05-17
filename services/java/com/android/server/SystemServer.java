8109cf8a4bf677f774a7a1ad494f0c28a40e89fe
Clark Scheff <clark@cyngn.com>
2014-05-16 18:32:58 -0700

    CM11 Themes: Allow defining a custom default theme [1/3]
    Change-Id: I6b794d6c9c27b2d6436bdf4fec51c84a7859c134
    
    Conflicts:
    	core/java/android/content/pm/ThemeUtils.java
    	core/java/android/provider/Settings.java
    	packages/SettingsProvider/res/values/defaults.xml
    	packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
    	services/java/com/android/server/ThemeService.java
    
    Conflicts:
    	packages/SettingsProvider/res/values/defaults.xml
    
    Conflicts:
    	core/java/android/content/pm/ThemeUtils.java
    	packages/SettingsProvider/res/values/defaults.xml

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1002,6 +1002,7 @@ class ServerThread {
         final PrintManagerService printManagerF = printManager;
         final MediaRouterService mediaRouterF = mediaRouter;
         final IPackageManager pmf = pm;
+        final ThemeService themeServiceF = themeService;
 
         // We now tell the activity manager it is okay to run third party
         // code.  It will call back into us once it has gotten to the state
@@ -1157,6 +1158,8 @@ class ServerThread {
                 }
 
                 try {
+                    // now that the system is up, apply default theme if applicable
+                    if (themeServiceF != null) themeServiceF.systemRunning();
                     CustomTheme customTheme = CustomTheme.getBootTheme(contextF.getContentResolver());
                     String iconPkg = customTheme.getIconPackPkgName();
                     pmf.updateIconMapping(iconPkg);
