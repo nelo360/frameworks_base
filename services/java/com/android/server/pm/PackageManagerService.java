cac72924949413798e614826ccc564562859c7ef
Andy Mast <andy@cyngn.com>
2014-05-22 15:40:10 -0700

    Themes: Map themed icon to app after upgrade
    Before this patch, applying an icon theme and then upgrading an app would
    cause the app to lose its themed icon.
    
    Change-Id: Ia76a33d0fb731720aa735e9dd9596bc5d2e75d9f
    
    Patchset: 2
    http://review.cyanogenmod.org/#/c/64571/

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -5020,6 +5020,13 @@ public class PackageManagerService extends IPackageManager.Stub {
             }
             // Add the new setting to mSettings
             mSettings.insertPackageSettingLPw(pkgSetting, pkg);
+
+            // Themes: handle case where app was installed after icon mapping applied
+            if (mIconPackHelper != null) {
+                int id = mIconPackHelper.getResourceIdForApp(pkg.packageName);
+                pkg.applicationInfo.themedIcon = id;
+            }
+
             // Add the new setting to mPackages
             mPackages.put(pkg.applicationInfo.packageName, pkg);
             // Make sure we don't accidentally delete its data.
@@ -5173,6 +5180,13 @@ public class PackageManagerService extends IPackageManager.Stub {
                 PackageParser.Activity a = pkg.activities.get(i);
                 a.info.processName = fixProcessName(pkg.applicationInfo.processName,
                         a.info.processName, pkg.applicationInfo.uid);
+
+                // Themes: handle case where app was installed after icon mapping applied
+                if (mIconPackHelper != null) {
+                    a.info.themedIcon = mIconPackHelper
+                            .getResourceIdForActivityIcon(a.info);
+                }
+
                 mActivities.addActivity(a, "activity");
                 if ((parseFlags&PackageParser.PARSE_CHATTY) != 0) {
                     if (r == null) {
@@ -12401,6 +12415,7 @@ public class PackageManagerService extends IPackageManager.Stub {
     }
 
     private void clearIconMapping() {
+        mIconPackHelper = null;
         for (Activity activity : mActivities.mActivities.values()) {
             activity.info.themedIcon = 0;
         }
