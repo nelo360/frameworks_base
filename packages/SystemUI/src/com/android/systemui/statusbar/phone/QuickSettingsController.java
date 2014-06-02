toyr99/android_frameworks_base

Quicksettings: add network speed indicator tile (1/2)

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -45,6 +45,7 @@ import static com.android.internal.util.mahdi.QSConstants.TILE_WIFI;
 import static com.android.internal.util.mahdi.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.mahdi.QSConstants.TILE_WIMAX;
 import static com.android.internal.util.mahdi.QSConstants.TILE_NAVBAR;
+import static com.android.internal.util.mahdi.QSConstants.TILE_NETWORKSPEED;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -94,6 +95,7 @@ import com.android.systemui.quicksettings.WiFiDisplayTile;
 import com.android.systemui.quicksettings.WiFiTile;
 import com.android.systemui.quicksettings.WifiAPTile;
 import com.android.systemui.quicksettings.NavBarTile;
+import com.android.systemui.quicksettings.NetworkSpeedTile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -251,6 +253,8 @@ public class QuickSettingsController {
                 }
 	    } else if (tile.equals(TILE_NAVBAR)) {
                 qs = new NavBarTile(mContext, this, mHandler);
+	    } else if (tile.equals(TILE_NETWORKSPEED)) {
+                qs = new NetworkSpeedTile(mContext, this, mHandler);
             } else if (tile.equals(TILE_NETWORKADB)) {
                 mTileStatusUris.add(Settings.Global.getUriFor(Settings.Global.ADB_ENABLED));
                 if (QSUtils.adbEnabled(resolver)) {

nelo360/frameworks_base

Conflicts:
	core/java/com/android/internal/util/mahdi/QSConstants.java
	core/java/com/android/internal/util/mahdi/QSUtils.java
	packages/SystemUI/res/values/mahdi_strings.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
Change-Id: Ic0513818d3ac91ef5e080a80399f4742a01da085

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -56,6 +56,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.slim.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.slim.QSConstants.TILE_PROFILE;
 import static com.android.internal.util.slim.QSConstants.TILE_COMPASS;
+import static com.android.internal.util.slim.QSConstants.TILE_NETWORKSPEED;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -112,6 +113,7 @@ import com.android.systemui.quicksettings.WiFiTile;
 import com.android.systemui.quicksettings.WifiAPTile;
 import com.android.systemui.quicksettings.RebootTile;
 import com.android.systemui.quicksettings.ProfileTile;
+import com.android.systemui.quicksettings.NetworkSpeedTile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -274,7 +276,9 @@ public class QuickSettingsController {
                 mTileStatusUris.add(Settings.System.getUriFor(Settings.System.SYSTEM_PROFILES_ENABLED));
                 if (QSUtils.systemProfilesEnabled(resolver)) {
                     qs = new ProfileTile(mContext, this);
-                }
+		}
+	    } else if (tile.equals(TILE_NETWORKSPEED)) {
+                qs = new NetworkSpeedTile(mContext, this, mHandler);
             } else if (tile.equals(TILE_COMPASS)) {
                 qs = new CompassTile(mContext, this);
             }