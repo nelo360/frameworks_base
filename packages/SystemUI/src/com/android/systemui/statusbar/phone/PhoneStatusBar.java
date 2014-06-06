4bfb03f2176ebd27049ef175cce044bc26bde96b
Andy Mast <andy@cyngn.com>
2014-06-06 13:40:08 -0700

    sync status bar transparency state on theme change
    When the status bar view is reinflated its transparency mode is
    reset back to opaque. This can be observed by changing a theme
    and switching to home in the middle of a switch.
    
    Change-Id: I3049c5f24cc6c46dcefb24188039a08503db09a9
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4251,7 +4251,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateExpandedViewPos(EXPANDED_LEAVE_ALONE);
 
         restorePieTriggerMask();
-
+        checkBarModes();
         mRecreating = false;
     }
 
