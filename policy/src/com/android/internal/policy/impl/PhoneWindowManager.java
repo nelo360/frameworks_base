6d7e862d5426cf0118cdce04e6097e5fe4d2ffee
Andy Mast <andy@cyngn.com>
2014-06-11 13:58:51 -0700

    Themes: PhoneWindowManager UI context on demand
    Creating the UI context in init causes some elements
    like the power menu to not theme correctly. This solution follows
    the same pattern as VolumePanel.
    
    Change-Id: I5c12f9a3bc8f5fe8d96ef2775b840926a6c35e28
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -929,11 +929,18 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             // Do the switch
             final AudioManager am = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
             final int ringerMode = am.getRingerMode();
-            final VolumePanel volumePanel = new VolumePanel(mUiContext,
+            final VolumePanel volumePanel = new VolumePanel(getUiContext(),
                                                               (AudioService) getAudioService());
             volumePanel.postVolumeChanged(AudioManager.STREAM_RING,AudioManager.FLAG_SHOW_UI);
         }
     };
+
+    private Context getUiContext() {
+        if (mUiContext == null) {
+            mUiContext = ThemeUtils.createUiContext(mContext);
+        }
+        return mUiContext != null ? mUiContext : mContext;
+    }
 
     void showGlobalActionsDialog(boolean pokeWakeLock) {
         if (mGlobalActions == null) {
@@ -1035,15 +1042,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     public void init(Context context, IWindowManager windowManager,
             WindowManagerFuncs windowManagerFuncs) {
         mContext = context;
-        mUiContext = ThemeUtils.createUiContext(context);
-        ThemeUtils.registerThemeChangeReceiver(context, new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                mUiContext = ThemeUtils.createUiContext(mContext);
-            }
-        });
-
-
         mWindowManager = windowManager;
         mWindowManagerFuncs = windowManagerFuncs;
         mHeadless = "1".equals(SystemProperties.get("ro.config.headless", "0"));
@@ -1208,6 +1206,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         + deviceKeyHandlerLib, e);
             }
         }
+
+        ThemeUtils.registerThemeChangeReceiver(context, new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                mUiContext = null;
+            }
+        });
     }
 
     private void updateKeyAssignments() {
