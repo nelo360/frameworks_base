a5cfe9c516983fb5f6d97e57a5ca64977d9eca43
Schubi <schubi@erlangen.ccc.de>
2014-06-17 09:44:15 +0200

    HOVER: Disabling HOVER isn't necessary at all [1/2] …
    as we can just hide the HOVER-Button in StatusBar
    
    Change-Id: Idde34acc859e6191a71239616b2b69f09650ef49
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -252,7 +252,6 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     // Hover
     protected Hover mHover;
-    protected boolean mHoverEnabled;
     protected boolean mHoverActive;
     protected boolean mHoverHideButton;
     protected ImageView mHoverButton;
@@ -502,15 +501,6 @@ public abstract class BaseStatusBar extends SystemUI implements
 	    SidebarObserver observer = new SidebarObserver(mHandler);
         observer.observe();
 
-        // Listen for HOVER enabled
-        mContext.getContentResolver().registerContentObserver(
-                Settings.System.getUriFor(Settings.System.HOVER_ENABLED),
-                        false, new ContentObserver(new Handler()) {
-            @Override
-            public void onChange(boolean selfChange) {
-                updateHoverActive();
-            }});
-
         // Listen for HALO state
         mContext.getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.HALO_ACTIVE), false, new ContentObserver(new Handler()) {
@@ -681,7 +671,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     }
 
     protected void updateHoverButton(boolean shouldBeVisible) {
-        mHoverButton.setVisibility((mHoverEnabled && !mHoverHideButton && shouldBeVisible) ? View.VISIBLE : View.GONE);
+        mHoverButton.setVisibility((shouldBeVisible && !mHoverHideButton) ? View.VISIBLE : View.GONE);
     }
 
     protected void updateHoverButton() {
@@ -689,18 +679,14 @@ public abstract class BaseStatusBar extends SystemUI implements
     }
 
     public void updateHoverActive() {
-        mHoverEnabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HOVER_ENABLED, 0) == 1;
-
-        mHoverActive = mHoverEnabled &&
-                Settings.System.getInt(mContext.getContentResolver(),
+        mHoverActive = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HOVER_ACTIVE, 0) == 1;
 
         mHoverHideButton = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HOVER_HIDE_BUTTON, 1) == 1;
 
         updateHoverButton();
-        if (mHoverEnabled) {
+        if (!mHoverHideButton) {
             mHoverButton.setImageResource(mHoverActive ?
                     R.drawable.ic_notify_hover_pressed : R.drawable.ic_notify_hover_normal);
         }
