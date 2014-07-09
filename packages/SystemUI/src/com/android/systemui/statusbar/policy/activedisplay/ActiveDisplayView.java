5f71a1edc3d593375fb6554086889cbcef0685c7
nelo360 <nelo360@gmail.com>
2014-07-09 13:19:24 +0200

    fixes after merge
    Change-Id: I546184318ee389c769b9f1f35c4ddb3dc2a44822

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -70,7 +70,6 @@ import android.widget.RemoteViews;
 import android.widget.TextView;
 
 import com.android.internal.util.slim.DeviceUtils;
-import com.android.internal.util.slim.QuietHoursHelper;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.internal.widget.multiwaveview.GlowPadView;
 import com.android.internal.widget.multiwaveview.GlowPadView.OnTriggerListener;
@@ -954,7 +953,8 @@ public class ActiveDisplayView extends FrameLayout
     }
 
     private boolean inQuietHoursDim() {
-        return QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_DIM);
+        return Settings.System.getIntForUser(mContext.getContentResolver(), Settings.System.QUIET_HOURS_DIM, 
+               0, UserHandle.USER_CURRENT_OR_SELF) == 2; 
     }
 
     private void onScreenTurnedOn() {
