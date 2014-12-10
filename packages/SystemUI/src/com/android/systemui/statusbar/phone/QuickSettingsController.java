dskim9752/android_frameworks_base

Quick Settings: Weather tile
You can check the weather to make it easier.

Dokdo Project - Gwon Hyeok

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -48,21 +48,31 @@ import static com.android.internal.util.cm.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFI;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.cm.QSConstants.TILE_WIMAX;
+import static com.android.internal.util.cm.QSConstants.TILE_WEATHER;
 
+import android.app.Activity;
+import android.app.ActivityManagerNative;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Handler;
+import android.os.RemoteException;
 import android.os.Message;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.WindowManager;
+import android.view.WindowManagerGlobal;
 
 import com.android.internal.util.cm.QSUtils;
 import com.android.systemui.quicksettings.AirplaneModeTile;
@@ -101,7 +111,10 @@ import com.android.systemui.quicksettings.VolumeTile;
 import com.android.systemui.quicksettings.RemoteDisplayTile;
 import com.android.systemui.quicksettings.WiFiTile;
 import com.android.systemui.quicksettings.WifiAPTile;
+import com.android.systemui.quicksettings.Weather;
+import com.android.systemui.R;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -283,6 +296,9 @@ public class QuickSettingsController {
                 qs = new MusicTile(mContext, this);
             } else if (tile.equals(TILE_INTERNALMEMORY)) {
                 qs = new InternalMemory(mContext, this);
+            } else if (tile.equals(TILE_WEATHER)) {
+                qs = new Weather(mContext, this, mHandler);
+                WeatherDialog();
             }
 
             if (qs != null) {
@@ -476,6 +492,50 @@ public class QuickSettingsController {
         mContainerView.updateResources();
         for (QuickSettingsTile t : mQuickSettingsTiles) {
             t.updateResources();
+        }
+    }
+
+    private void WeatherDialog() {
+        int check = filecheck("/sdcard/Android/data/weather.txt");
+        if ( check == 0 ) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setPositiveButton(R.string.weather_ok, new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+
+            builder.setNegativeButton(R.string.weather_link, new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                 Intent intent = new Intent();
+                 intent.setClassName("com.cyanogenmod.lockclock", "com.cyanogenmod.lockclock.preference.Preferences");
+                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                 mContext.startActivity(intent);
+                 }
+            });
+        builder.setMessage(R.string.weather_dialog_msg);
+        builder.setTitle(R.string.weather_notify);
+        builder.setCancelable(true);
+        final Dialog dialog = builder.create();
+        dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        try {
+            WindowManagerGlobal.getWindowManagerService().dismissKeyguard();
+        } catch (RemoteException e) {
+        }
+        dialog.show();
+        }
+    }
+
+    public  int filecheck(String PATH) {
+        File f = new File(PATH);
+        int isfile;
+        if (f.isFile()) {
+           isfile = 1;
+        return isfile;
+        } else {
+          isfile = 0 ;
+        return isfile;
         }
     }
 }

nelo360/frameworks_base

Conflicts:
	packages/SystemUI/res/values/dokdo_strings.xml
	packages/SystemUI/res/values/styles.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
	packages/SystemUI/res/values/slim_strings.xml
Change-Id: I0eeb8abdcc9bc1f6a8e957f0835d4367083c8268
	packages/SystemUI/res/values-es/slim_strings.xml

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -57,16 +57,24 @@ import static com.android.internal.util.slim.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.slim.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.slim.QSConstants.TILE_REMOTEDISPLAY;
 import static com.android.internal.util.slim.QSConstants.TILE_PROFILE;
+import static com.android.internal.util.slim.QSConstants.TILE_WEATHER;
 
+import android.app.Activity;
+import android.app.ActivityManagerNative;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Handler;
+import android.os.RemoteException;
 import android.os.Message;
 import android.os.SystemProperties;
 import android.os.UserHandle;
@@ -77,6 +85,8 @@ import android.view.LayoutInflater;
 import com.android.internal.util.slim.QSUtils;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
+import android.view.WindowManagerGlobal;
 import com.android.internal.util.slim.DeviceUtils;
 import com.android.systemui.R;
 import com.android.systemui.quicksettings.AirplaneModeTile;
@@ -119,6 +129,9 @@ import com.android.systemui.quicksettings.RebootTile;
 import com.android.systemui.quicksettings.ProfileTile;
 import com.android.systemui.statusbar.phone.QuickSettingsContainerView.QSSize;
 
+import com.android.systemui.quicksettings.Weather;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -288,7 +301,10 @@ public class QuickSettingsController {
                 mTileStatusUris.add(Settings.System.getUriFor(Settings.System.SYSTEM_PROFILES_ENABLED));
                 if (QSUtils.systemProfilesEnabled(resolver)) {
                     qs = new ProfileTile(mContext, this);
-                }
+		}
+            } else if (tile.equals(TILE_WEATHER)) {
+                qs = new Weather(mContext, this, mHandler);
+                WeatherDialog();
             }
 
             if (qs != null) {
@@ -528,4 +544,49 @@ public class QuickSettingsController {
             t.setImageMargins(margin);
         }
     }
+
+    private void WeatherDialog() {
+        int check = filecheck("/sdcard/Android/data/weather.txt");
+        if ( check == 0 ) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setPositiveButton(R.string.weather_ok, new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+
+            builder.setNegativeButton(R.string.weather_link, new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                 Intent intent = new Intent();
+                 intent.setClassName("com.cyanogenmod.lockclock", "com.cyanogenmod.lockclock.preference.Preferences");
+                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                 mContext.startActivity(intent);
+                 }
+            });
+        builder.setMessage(R.string.weather_dialog_msg);
+        builder.setTitle(R.string.weather_notify);
+        builder.setCancelable(true);
+        final Dialog dialog = builder.create();
+        dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        try {
+            WindowManagerGlobal.getWindowManagerService().dismissKeyguard();
+        } catch (RemoteException e) {
+        }
+        dialog.show();
+        }
+    }
+
+    public  int filecheck(String PATH) {
+        File f = new File(PATH);
+        int isfile;
+        if (f.isFile()) {
+           isfile = 1;
+        return isfile;
+        } else {
+          isfile = 0 ;
+        return isfile;
+	}
+    }
+
 }