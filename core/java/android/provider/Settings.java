9d06d930e0d62706327b5c4a9ba35da4e5f2a200
nelo360 <nelo360@gmail.com>
2014-12-25 00:35:54 +0100

    fix statusbar notification count
    Change-Id: Iba464164076120482dd3cddd0431fa90ed23aad2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4397,6 +4397,12 @@ public final class Settings {
          */
         public static final String PEEK_SCREEN_TIMEOUT = "peek_screen_timeout";
 
+	/**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
